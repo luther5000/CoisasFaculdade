2412f65c67eaf163fb281fae2b4e1cefb3b310f0
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-10-27 10:33:32 -0300

    Update projeto.py

--- ProjetoPO/projeto.py
+++ ProjetoPO/projeto.py
@@ -1,88 +1,132 @@
 import math
 
 from mip import *
-import numpy as py
-
-def solve(model):
-    status = model.optimize()
-
-    print("Status = ", status)
-    if status != OptimizationStatus.OPTIMAL:
-        return
-
-    print(f"Solution value  = {model.objective_value:.2f}\n")
-
-    print("Solution:")
-    for v in model.vars:
-        if v.x > 0.001:
-            print(f"{v.name} = {v.x:.2f}")
 
 
-def criaModel(numVar, numRest, objetivo, restricoes):
-    model = Model(sense=MAXIMIZE, solver_name=CBC)
+class Modelo:
+    def __init__(self):
+        self.menor = 0
+        self.maior = 0
 
-    x = [model.add_var(var_type=CONTINUOUS, lb=0, up=1, name="x_" + str(i)) for i in range(numVar)]
+    def save(self, model, filename):
+        model.write(filename)
+        with open(filename, "r") as f:
+            print(f.read())
 
-    objetivo = objetivo.split(' ')
+    def solve(self, model):
+        status = model.optimize()
 
-    model.objective = xsum(int(objetivo[i]) * x[i] for i in range(numVar))
+        print("Status = ", status)
+        if status != OptimizationStatus.OPTIMAL:
+            return
 
+        print(f"Solution value  = {model.objective_value:.2f}\n")
 
-    for i in range(numRest):
-        linhaAtual = restricoes[i]
-        model += xsum(int(linhaAtual[j]) * x[j] for j in range(numVar - 1)) <= int(linhaAtual[numVar])
+        print("Solution:")
+        for v in model.vars:
+            if v.x > 0.001:
+                print(f"{v.name} = {v.x:.2f}")
 
-    return model
+    def criaModel(self, numVar, numRest, objetivo, restricoes):
+        model = Model(sense=MAXIMIZE, solver_name=CBC)
 
+        x = [model.add_var(var_type=CONTINUOUS, lb=0, ub=1, name="x_" + str(i)) for i in range(numVar)]
 
-def executaTudo(model, y, s):
-    if s == '<=':
-        cont = 0
+        #for i in objetivo:
+            #i.strip()
+
+        model.objective = xsum(int(objetivo[i]) * x[i] for i in range(numVar))
+
+        for i in range(numRest):
+            linhaAtual = restricoes[i].split()
+            model += xsum(int(linhaAtual[j]) * x[j] for j in range(numVar)) <= int(linhaAtual[numVar])
+
+        self.save(model, "model.lp")
+        return model
+
+    def executaTudo(self, model, y, s):
+        if s == '1':
+            cont = 0
+            for i in model.vars:
+                if i.name == y:
+                    model += model.var_by_name("x_" + str(cont)) == 1
+                cont += 1
+        else:
+            cont = 0
+            for i in model.vars:
+                if i.name == y:
+                    model += model.var_by_name("x_" + str(cont)) == 0
+                cont += 1
+
+        if model.optimize() != OptimizationStatus.OPTIMAL:
+            return
+
+        self.save(model, "model.lp")
+        self.solve(model)
+
+        if self.menor != 0:
+            if model.objective_value <= self.menor:
+                return
+
+        lista = []
         for i in model.vars:
-            if str(i) == y:
-                model += model.var_by_name("x[" + str(cont) + "]") <= math.floor(i)
-            ++cont
-    else:
-        cont = 0
-        for i in model.vars:
-            if str(i) == y:
-                model += model.var_by_name("x[" + str(cont) + "]") >= round(i)
-            ++cont
+            if round(i.x) != i.x:
+                lista.append(i)
 
-    solve(model)
-    # adicionar if da solução atual ser menor do que a melhor solução já achada
-    # adicionar o if da terceira condição d saida q eu n lembro qual eh
+        if len(lista) != 0:
+            menorDist = 10
+            indice = -1
+            for i in range(len(lista)):
+                if i - math.floor(i) - 0.5 < menorDist:
+                    indice = i
+                    menorDist = i - math.floor(i) - 0.5
+            self.executaTudo(model, str(lista[indice]), "1")
+            self.executaTudo(model, str(lista[indice]), "0")
 
-    lista = []
-    for i in model.vars:
-        if round(i) != i:
-            lista.append(i)
-
-    if lista.__sizeof__() != 0:
-        menorDist = 10
-        for i in range (lista.__sizeof__()):
-            if i-math.floor(i) - 0.5 < menorDist:
-                indice = i
-                menorDist = i-math.floor(i) - 0.5
-        executaTudo(model, str(lista[indice]), "<=")
-        executaTudo(model, str(lista[indice]), ">=")
+        else:
+            if self.menor != 0:
+                if self.menor < model.objective_value:
+                    self.menor = model.objective_value
+                return
+            else:
+                self.menor = model.objective_value
+                return
 
 
 linhas = []
-with open("arquivo.txt") as file:
+with open("teste1.txt") as file:
     for line in file:
         linhas.append(line)
 
-linhaAtual = linhas[0].split(' ')
+linhaAtual = linhas[0].split()
 numVar = int(linhaAtual[0])
 numRest = int(linhaAtual[1])
 
+objetivo = linhas[1].split()
 
-objetivo = linhas[1].split(' ')
 
 restricoes = []
 for i in range(numRest):
-    restricoes.append(linhas[i+2])
+    restricoes.append(linhas[i + 2])
 
-model = criaModel(numVar, numRest, objetivo, restricoes)
-solve(model)
+modelo = Modelo()
+model = modelo.criaModel(numVar, numRest, objetivo, restricoes)
+modelo.solve(model)
+
+lista = []
+for i in model.vars:
+    if round(i.x) != i.x:
+        lista.append(i)
+
+if len(lista) != 0:
+    modelo.maior = model.objective_value
+    menorDist = 10
+    indice = -1
+    for i in range(len(lista)):
+        if i - math.floor(i) - 0.5 < menorDist:
+            indice = i
+            menorDist = i - math.floor(i) - 0.5
+    modelo.executaTudo(model, str(lista[indice]), "1")
+    modelo.executaTudo(model, str(lista[indice]), "0")
+
+    print(modelo.menor)
