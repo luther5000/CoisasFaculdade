039a2283dbec1894bcb653dd57f3b7fdaa9c9257
luther5000 <129391077+luther5000@users.noreply.github.com>
2024-03-25 15:40:22 -0300

    Update Buffer.java

--- SO/threadsSynchronized/Buffer.java
+++ SO/threadsSynchronized/Buffer.java
@@ -1,12 +1,13 @@
 public class Buffer {
     private String[] buffer;//o buffer propriamente dito
     private final int tamBuffer;//tamanho do buffer
-    private int proxInsercao;//o próximo lugar onde deve ser inserido no buffer
+    private int proxInsercao;//o proximo lugar onde deve ser inserido no buffer
     private int proxLeitura;//o proximo lugar onde deve ser lido no buffer
-    private int quantNoBuffer;//quantas coisas estão no buffer
-    private boolean[] flags;//flag para guardar a informação se determinada variável já consumiu ou não aquela palavra
-    private boolean acabou;//boolean que informa ao buffer se não há mais leituras a serem realizadas
-    private final int numThreads;//a quantidade de threads consumidoras que irão acessar o buffer
+    private int quantNoBuffer;//quantas coisas estao no buffer
+    private boolean[] flagsConsumidor;//flag para guardar a informacao se determinada variavel ja consumiu ou nao aquela palavra
+    private boolean acabou;//boolean que informa ao buffer se nao ha mais leituras a serem realizadas
+    private final int numThreads;//a quantidade de threads consumidoras que irao acessar o buffer
+    private int qualProdutor;
 
     public Buffer(int tam, int numThreads){
         buffer = new String[tam];
@@ -14,46 +15,51 @@ public class Buffer {
         proxInsercao = 0;
         proxLeitura = 0;
         quantNoBuffer = 0;
-        flags = new boolean[numThreads];
+        flagsConsumidor = new boolean[numThreads];
+        qualProdutor = -1;
         this.numThreads = numThreads;
         acabou = false;
     }
 
-    synchronized boolean insereBuffer(String palavra) {
-        //verificação para saber se o buffer está cheio
+    synchronized boolean insereBuffer(String palavra, int id) {
+        //verificacao para saber se o buffer esta cheio
         if(this.quantNoBuffer + 1 > this.tamBuffer)
             return false;
 
-        //insere no buffer
-        buffer[proxInsercao] = palavra;
+        if (qualProdutor == -1 || qualProdutor == id) {
+            qualProdutor = id;
+            //insere no buffer
+            buffer[proxInsercao] = palavra;
 
-        /* atualiza o proximo local a ser inserido e quantas coisas estão
-         * no buffer */
-        proxInsercao = (proxInsercao + 1) % tamBuffer;
-        ++quantNoBuffer;
+            /* atualiza o proximo local a ser inserido e quantas coisas estao
+             * no buffer */
+            proxInsercao = (proxInsercao + 1) % tamBuffer;
+            ++quantNoBuffer;
 
-        return true;
+            return true;
+        }
+        return false;
     }
 
     synchronized String getFromBuffer(int id){
-        //verifica se há mais algo a ser lido
+        //verifica se ha mais algo a ser lido
         if (acabou){
             return "-1";
         }
-        /*verifica se essa thread já leu a palavra atual e se
+        /*verifica se essa thread ja leu a palavra atual e se
          * tem algo no buffer para ser lido atualmente */
-        if (flags[id] || this.quantNoBuffer <= 0){
+        if (flagsConsumidor[id] || this.quantNoBuffer <= 0){
             return "";
         }
 
-        //atualiza a variável mostrando que essa thread já leu a palavra atual
-        flags[id] = true;
+        //atualiza a variavel mostrando que essa thread ja leu a palavra atual
+        flagsConsumidor[id] = true;
 
         //retorna para a thread a palavra
         return buffer[proxLeitura];
     }
 
-    /* nessa função sera feita a limpeza do buffer e a atualizacao
+    /* nessa funcao sera feita a limpeza do buffer e a atualizacao
      * das flags das threads consumidoras permitindo que elas possam
      * voltar a consumir */
     synchronized void clearBuffer(){
@@ -63,24 +69,28 @@ public class Buffer {
 
         //atualiza a flag
         for (int i = 0; i < this.numThreads; ++i)
-            flags[i] = false;
+            flagsConsumidor[i] = false;
 
         //quebra linha para deixar a saída organizada
         System.out.println();
     }
 
-    /* função que retorna se todas as threads já consumiram a
+    /* funcao que retorna se todas as threads ja consumiram a
      * palavra atual do buffer. serve para que a main saiba a hora
      * de limpar o buffer */
     synchronized boolean todasConsumiram(){
         boolean todasConsumiram = true;
         for (int i = 0; i < this.numThreads; ++i)
-            todasConsumiram = todasConsumiram && flags[i];
+            todasConsumiram = todasConsumiram && flagsConsumidor[i];
 
         return todasConsumiram;
     }
     synchronized void acabou(){
         this.acabou = true;
+    }
+
+    synchronized void produziTudo(){
+        this.qualProdutor = -1;
     }
 
     public int getQuantNoBuffer (){
