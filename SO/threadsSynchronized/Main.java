d374fce620b67f6eb06df0e417b44a2d6f27b3fb
luther5000 <129391077+luther5000@users.noreply.github.com>
2024-03-25 15:39:59 -0300

    Update Main.java

--- SO/threadsSynchronized/Main.java
+++ SO/threadsSynchronized/Main.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        
+
         /* criacao das strings que serao enviadas para os produtores
-        * obs: as frases foram geradas pelo chatGPT por falta de 
-        * criatividade minha para criar frases que não tenham
-        * palavras repetidas*/
+         * obs: as frases foram geradas pelo chatGPT por falta de
+         * criatividade minha para criar frases que nao tenham
+         * palavras repetidas*/
         ArrayList<String> mensagens = new ArrayList<>();
-        mensagens.add("O sol brilha no céu azul");
+        mensagens.add("O sol brilha no ceu azul");
         mensagens.add("A chuva cai suavemente sobre a terra molhada");
-        mensagens.add("Os pássaros voam livremente entre as árvores verdes");
+        mensagens.add("Os passaros voam livremente entre as arvores verdes");
         mensagens.add("A lua ilumina a noite escura com seu brilho prateado");
+        /*mensagens.add("As flores perfumam os campos");
+        mensagens.add("Os lirios do campo florescem delicadamente ao amanhecer");*/
 
         Buffer buffer = new Buffer(7, 4);
         int[] ids = {0, 1, 2, 3};
@@ -25,13 +27,13 @@ public class Main{
         }
 
         for (int i = 0; i < 4; ++i){
-            produtor[i].executaProdutor(mensagens.get(i), buffer);
+            produtor[i].executaProdutor(mensagens.get(i), buffer, ids[i]);
             consumidor[i].executaConsumidor(buffer, ids[i]);
         }
 
         do {
             /* vamos aguardar todas as threads consumidoras terem lido
-            * uma palavra para podermos fazer a limpeza no buffer */
+             * uma palavra para podermos fazer a limpeza no buffer */
             if (!buffer.todasConsumiram()){
                 try {
                     Thread.sleep(200);
@@ -44,10 +46,12 @@ public class Main{
             /* agora que todas consumiram iremos limpar o buffer */
             buffer.clearBuffer();
 
-            //tudo será feito enquanto houverem caracteres no buffer
-        } while (buffer.getQuantNoBuffer() > 0);
+            //tudo sera feito enquanto houverem caracteres no buffer
+        } while (buffer.getQuantNoBuffer() > 0 || produtor[0].isAlive() ||
+                produtor[1].isAlive() || produtor[2].isAlive() ||
+                produtor[3].isAlive());
 
-        /* informamos ao buffer que não há mais leituras a serem realizadas */
+        /* informamos ao buffer que nao ha mais leituras a serem realizadas */
         buffer.acabou();
 
         //vamos esperar as threads consumidoras acabarem
@@ -61,30 +65,40 @@ public class Main{
 }
 
 class Produtor extends Thread{
-    public void executaProdutor(String mensagem, Buffer buffer){
-        //separamos a mensagem toda em palavras 
+    public void executaProdutor(String mensagem, Buffer buffer, int id){
+        //separamos a mensagem toda em palavras
         String[] mensagemSeparada = mensagem.split(" ");
-        
+
         //criamos a funcao que sera executada pelas threads produtoras
         Runnable runnable = () -> {
-            
-            for (int i = 0; i < mensagemSeparada.length; ++i) {
-                //tentamos fazer a adição no buffer 
-                boolean foiInserido = buffer.insereBuffer(mensagemSeparada[i]);
-                
-                /* caso não tenha sido inserido decrescemos o 
-                 * contador em um para podermos tentar inserir essa 
-                 * palavra novamente*/
-                if (!foiInserido)
-                    --i;
-                
-                //a thread dorme por 200 ms
-                try {
-                    Thread.sleep(200);
-                } catch (Exception e){continue;}
+
+            while (!Thread.currentThread().isInterrupted()) {
+                boolean foiInserido = buffer.insereBuffer(mensagemSeparada[0], id);
+                if (foiInserido){
+                    for (int i = 1; i < mensagemSeparada.length; ++i){
+                        foiInserido = buffer.insereBuffer(mensagemSeparada[i], id);
+
+                        if (!foiInserido){
+                            --i;
+                            try {
+                                Thread.sleep(150);
+                            } catch (Exception e){
+                                continue;
+                            }
+                        }
+                    }
+                    Thread.currentThread().interrupt();
+                    buffer.produziTudo();
+                } else {
+                    try{
+                        Thread.sleep(300);
+                    } catch (Exception e){
+                        continue;
+                    }
+                }
             }
         };
-        
+
         //criamos a thread e a executamos
         Thread thread = new Thread(runnable);
         thread.start();
@@ -93,28 +107,28 @@ class Produtor extends Thread{
 
 class Consumidor extends Thread {
     public void executaConsumidor(Buffer buffer, int id) {
-        /* esse id recebido pela main servirá para a identificacao
-        * da thread no buffer especialmente */
-        
-        //criamos a função que sera executada pelas threads consumidoras
+        /* esse id recebido pela main servira para a identificacao
+         * da thread no buffer especialmente */
+
+        //criamos a funcao que sera executada pelas threads consumidoras
         Runnable runnable = () -> {
-            //a thread irá executar enquanto não for interrompida
+            //a thread ira executar enquanto nao for interrompida
             while (!Thread.currentThread().isInterrupted()) {
                 //recebemos a mensagem do buffer
                 String mensagem = buffer.getFromBuffer(id);
 
-                /* caso essa mensagem seja -1, quer dizer que é para
-                * a thread interromper sua execução porque acabou as 
-                * palavras para serem lidas */
+                /* caso essa mensagem seja -1, quer dizer que e para
+                 * a thread interromper sua execucao porque acabou as
+                 * palavras para serem lidas */
                 if (mensagem.equals("-1")) {
                     interrupt();
                     continue;
                 }
 
                 /* caso a mensagem recebida seja vazia, quer dizer que ou
-                * a thread atual já leu a palavra ou nao ha nada no 
-                * buffer no momento para ser lido, então a thread espera 
-                * um pouco antes de continuar sua execucao */
+                 * a thread atual ja leu a palavra ou nao ha nada no
+                 * buffer no momento para ser lido, entao a thread espera
+                 * um pouco antes de continuar sua execucao */
                 if (mensagem.isEmpty()) {
                     try {
                         Thread.sleep(200);
@@ -125,7 +139,7 @@ class Consumidor extends Thread {
                 }
 
                 /* caso a thread nao entre em nenhum dos ifs, a mensagem
-                * recebida era o esperado e será imprimido pela thread */ 
+                 * recebida era o esperado e sera imprimido pela thread */
                 System.out.print("ID '" + getId() + "': " + mensagem + " | ");
             }
         };
